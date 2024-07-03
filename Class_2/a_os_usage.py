44d52d4cba2ffea26745c86aa2fcf784682936cb
vaishnabala <vaishnavibalajivit@gmail.com>
2024-07-03 13:55:36 +0530

    Updated

--- Class_2/a_os_usage.py
+++ Class_2/a_os_usage.py
@@ -1,5 +1,6 @@
+# working with current working directories
 
 import os
 print (os.getcwd())
-#os.chdir("D:/User/")  # input your desired path and remove comment line at the beginningn
+#os.chdir("D:\Python\")  # input your desired path and remove comment tag at the beginning
 print (os.getcwd()) # try printing it
